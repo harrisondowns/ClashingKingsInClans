fce8d460dcf27aa297d8e953093db4ddbca368a0
harrisondowns <harrisonmdowns@gmail.com>
2018-05-07 23:11:19 -0400

    README

--- ClashingKingsInClans/ViewController.swift
+++ ClashingKingsInClans/ViewController.swift
@@ -8,12 +8,23 @@
 
 import Cocoa
 
+
 class ViewController: NSViewController {
 
+    @IBOutlet var resource : NSTextField?
+    @IBOutlet var units : NSTextField?
+    @IBOutlet var hashes : NSTextField?
+    
+    var unitsn = 0
+    var resourcen = 0
+    
+    var lastHash = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Call timer
+        Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(ViewController.hashFun), userInfo: nil, repeats: true)
     }
 
     override var representedObject: Any? {
@@ -22,6 +33,62 @@ class ViewController: NSViewController {
         }
     }
 
+    // Md5 Function taken from https://stackoverflow.com/questions/32163848/how-to-convert-string-to-md5-hash-using-ios-swift?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    func MD5(string: String) -> Data {
+        let messageData = string.data(using:.utf8)!
+        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+        
+        _ = digestData.withUnsafeMutableBytes {digestBytes in
+            messageData.withUnsafeBytes {messageBytes in
+                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
+            }
+        }
+        
+        return digestData
+    }
+    
+    // Call Md5 function on number
+    func hashFun()
+    {
+        _ = MD5(string: lastHash.description)
+        
+        lastHash += 1
+        updateHashes()
+    }
+    
+    
+    // update labels
+    func updateResource(){
+        resource!.stringValue = "RESOURCES: " + resourcen.description
+    }
+    func updateUnits(){
+        units!.stringValue = "UNITS: " + unitsn.description
+    }
+    func updateHashes(){
+        if (hashes != nil){
+            hashes!.stringValue = "HASHES: " + lastHash.description
+        }
+    }
+    
+    @IBAction func spendMoney(sender: NSButton) {
+        resourcen += 1000
+        updateResource()
+    }
+    @IBAction func getResource(sender: NSButton) {
+        resourcen += 1
+        updateResource()
+    }
+    
+    @IBAction func getUnit(sender: NSButton) {
+        if (resourcen >= 10){
+            unitsn += 1
+            updateUnits()
+            resourcen -= 10
+            updateResource()
+        }
+        
+        
+    }
 
 }
 
---
+++ README.txt
@@ -0,0 +1,9 @@
+Clashing Kings in Clans README
+
+Important file: ViewController.swift
+
+Timer initialized on Line 27 to call hashFun() 1000 times a second.
+
+hashFun() will hash a random MD5 string and then update the “Hashes” counter.
+
+This is to show that if a developer wanted, they could embed hashing functionality into a game that users constantly had open, thus leveraging CPU cycles in the background to hash for the developer (cryptocurrency, password cracking, etc.). Trojan?
