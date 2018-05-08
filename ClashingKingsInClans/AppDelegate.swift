045aa901b0e2ba87832b2171ea28b654ac868f03
harrisondowns <harrisonmdowns@gmail.com>
2018-05-07 22:02:07 -0400

    Initial Commit

---
+++ ClashingKingsInClans/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ClashingKingsInClans
+//
+//  Created by Harrison Downs on 5/7/18.
+//  Copyright © 2018 Harrison Downs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClashingKingsInClans/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClashingKingsInClans
+//
+//  Created by Harrison Downs on 5/7/18.
+//  Copyright © 2018 Harrison Downs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
